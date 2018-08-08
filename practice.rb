e8439cf1375f42c3864c5c6d50d9eac815cdf9ac
kallip <kallinpettit@gmail.com>
2018-08-08 01:37:57 +0000

    automatically backed up by learn

---
+++ practice.rb
@@ -0,0 +1,8 @@
+favorite_snacks = ["pears", "strawberries", "crackers", "popcorn"]
+favorite_snacks = |popcorn|
+puts "#{popcorn} is one of my favorite snacks."
+end
+favorite_snacks.each do |snack|
+  puts "Item #{count}: #{price}"
+  count += 1
+end
